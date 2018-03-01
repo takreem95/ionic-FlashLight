caa4f7e37f4c4dafb38fa74746196e10fb96e874
Tazeem- Baig <btazeem@gmail.com>
2018-03-01 03:10:41 -0800

    ionic-FlashLight

---
+++ src/pages/home/home.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  providers: [Flashlight]
+})
+export class HomePage {
+  buttonColor: string = '#000'; //Default Color
+  btn_title: any;
+  constructor(public navCtrl: NavController, private fl: Flashlight) {
+    this.btn_title = 'Toggle Flashlight';
+  }
+  switchFlashLight(){
+    this.fl.available().then((resp) => {
+      this.fl.toggle().then(data => {
+        console.log(data)
+        this.buttonColor = '#345465';
+      }).catch(err => {
+        console.log(err)
+      })
+    }).catch(err => {
+      console.log(err)
+      alert('FlashLight is not available');
+    });
+  }
+
+}
